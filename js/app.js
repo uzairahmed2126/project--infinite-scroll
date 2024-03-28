11437710f104bdd524bea65a92dd6fa052d88695
uzairahmed2126 <uzairahmed21261@gmail.com>
2024-03-28 18:17:17 +0530

    completed

--- js/app.js
+++ js/app.js
@@ -1,22 +1,25 @@
 const mainContainer = document.getElementById('main-container');
-window.addEventListener('scroll', handleScrollEvent);
 
-function createDiv(element, className) {
-    const div = document.createElement(element);
-    div.className = className;
-    mainContainer.append(div);
+function createEl(element, src, className, fragment) {
+    const img = document.createElement(element);
+    img.className = className;
+    img.src = src;
+    fragment.append(img);
 }
 function scrollEvent() {
-    for (let i = 0; i <= 8; i++) {
-        const div1 = createDiv('div', 'boxes');
+    const domFragment = document.createDocumentFragment();
+    for (let i = 1; i <= 9; i++) {
+        const div1 = createEl('img', `https://picsum.photos/seed/picsum${Math.floor(Math.random(i) * 1000)}/400/350`, 'images', domFragment);
     }
+    mainContainer.append(domFragment);
 }
 scrollEvent();
 
 function handleScrollEvent(e) {
-    const scrollHeight = window.pageYOffset;
-    if (scrollHeight > 122) {
+    if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
         scrollEvent();
     }
-    console.log(scrollHeight)
+    console.log(window.innerHeight);
 }
+
+window.addEventListener('scroll', handleScrollEvent);
